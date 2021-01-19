215da80a0b5832df06647f8b5d0752311ea419cd
HHHUUUGGGOOO <hugo551565@gmail.com>
2021-01-19 23:26:12 +0800

    init commit

---
+++ token_file/token.py
@@ -0,0 +1,13 @@
+#############################################################################
+# FileName     [ token.py ]
+# PackageName  [ token_file ]
+# Synopsis     [ access token ]
+# Author       [ Meng-Hung (Hugo), Chen ]
+# Copyright    [ Copyleft(c) 2021 ]
+#############################################################################
+
+#############################################################################
+#                               parameter                                   #
+#############################################################################
+CHANNEL_ACCESS_TOKEN = 'TH5M6XYWvCMXeViq6bhtxWD1bnsyfadUrC57X01/pCq1bq1WSfqlL4OqLpRFm+DwKMtsLqXGVn2xr3ehydWnaPMga4dbAYEraAVOpNLxqW2RTo/9TNbTIc7iHHD+clyv85V3R2SNJYsKJslg4V79IQdB04t89/1O/w1cDnyilFU='
+CHANNEL_SECRET = '21d018bdcbe3cbc1e542c7f6688c1e71'
