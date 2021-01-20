beb50e219d1bfb6eeb460e1d9b27a3691f237f42
HHHUUUGGGOOO <hugo551565@gmail.com>
2021-01-20 11:34:43 +0800

    final

--- message.py
+++ message.py
@@ -69,7 +69,7 @@ def text_message(user: User, msg: str):
         messages = list()
         messages.append(TextSendMessage(text='🙋‍♂️Hi！I am Hugo Chen, an undergraduate studied in NTUEE.\n📸The attached video is my brief introduction！\n😇If wanna know me more, please click your interested section below！'))
         messages.append(VideoSendMessage(
-            original_content_url = 'https://streamable.com/psvw92',
+            original_content_url = 'https://i.imgur.com/siLRci5.mp4',
             preview_image_url = 'https://i.imgur.com/RtuVLZQ.jpg'
         ))        
         return messages
@@ -83,7 +83,7 @@ def follow_event_message(user: User):
     messages = list()
     messages.append(TextSendMessage(text='🙋‍♂️Hi！I am Hugo Chen, an undergraduate studied in NTUEE.\n📸The attached video is my brief introduction！\n😇If wanna know me more, please click your interested section below！'))
     messages.append(VideoSendMessage(
-        original_content_url = 'https://streamable.com/psvw92',
+        original_content_url = 'https://i.imgur.com/siLRci5.mp4',
         preview_image_url = 'https://i.imgur.com/RtuVLZQ.jpg',
         quick_reply=quick_reply.get_quick_reply(['Finish！'])
     ))
